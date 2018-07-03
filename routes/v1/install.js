3417c0dd22385f972c19b00afde72a6fd2ea1c02
Michael Borglin <borglin.me@gmail.com>
2018-07-03 12:08:20 -0700

    created generate install/application route

--- routes/v1/install.js
+++ routes/v1/install.js
@@ -73,4 +73,49 @@ router.post('/chain/:name/:chain', function (req, res) {
     .catch(handleError);
 });
 
+router.options('/application');
+router.post('/application/:name/', function (req, res) {
+
+  const name = req.params.name;
+
+  try {
+    validator.isAlphanumeric(name);
+  } catch (error) {
+    res.status(400).json(error);
+    return;
+  }
+
+  function ensureOneApplicationAvailable(applications) {
+    if(applications.length === 0) {
+      throw {
+        code: 'NO_APPLICATION_FOUND',
+        text: 'There are no applications that meet the given specifications.'
+      }
+    } else if(applications.length > 1) {
+      throw {
+        code: 'MULTIPLE_APPLICATIONS_FOUND',
+        text: 'Multiple applications meet the given specifications. Please be more specific.'
+      }
+    }
+
+    return applications[0];
+  }
+
+  function handleSuccess(applicationNames) {
+    res.json(applicationNames);
+  }
+
+  function handleError(error) {
+    var stringError = 'Unable to find any available applications';
+    logger.error(stringError, 'install', error);
+    res.status(500).json(stringError);
+  }
+
+  applicationLogic.getAvailable(name)
+    .then(ensureOneApplicationAvailable)
+    .then(applicationLogic.install)
+    .then(handleSuccess)
+    .catch(handleError);
+});
+
 module.exports = router;
