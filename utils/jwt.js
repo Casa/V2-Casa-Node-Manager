a4e55f36943a9c9a911d8451ff6f5a9fde498ffe
Stacie Waleyko <stacie@team.casa>
2019-02-25 12:20:22 -0500

    Revert "Fix issue where user ID was not getting into the JWT (does it matter?)"
    This reverts commit 55d028386bfb26acb30a1e34b53943655cfed14a.

--- utils/jwt.js
+++ utils/jwt.js
@@ -6,22 +6,18 @@ const diskLogic = require('logic/disk.js');
 const expiresIn = process.env.JWT_EXPIRATION ? parseInt(process.env.JWT_EXPIRATION, 3600) : 3600;
 
 async function generateJWT(account) {
+
   const jwtPrivateKey = await diskLogic.readJWTPrivateKeyFile();
 
   const jwtPubKey = await diskLogic.readJWTPublicKeyFile();
 
-  let token;
+  // eslint-disable-next-line object-shorthand
+  const token = await jwt.sign({id: account}, jwtPrivateKey, {expiresIn: expiresIn, algorithm: 'RS256'});
 
-  // Use await to make sure the account name resolves before putting it in the JWT
-  await account.then(async res => {
-    // eslint-disable-next-line object-shorthand
-    token = await jwt.sign({id: res}, jwtPrivateKey, {expiresIn: expiresIn, algorithm: 'RS256'});
-
-    await jwt.verify(token, jwtPubKey, function(error) {
-      if (error) {
-        return Promise.reject(new Error('Error generating JWT token.'));
-      }
-    });
+  await jwt.verify(token, jwtPubKey, function(error) {
+    if (error) {
+      return Promise.reject(new Error('Error generating JWT token.'));
+    }
   });
 
   return token;
