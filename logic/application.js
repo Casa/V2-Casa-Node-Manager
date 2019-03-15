b144990a82d03ba7f4a3196a21aa9f4793e10e56
Michael Borglin <michael@team.casa>
2019-03-15 09:42:37 -0700

    refactor logs logic to seperate file

--- logic/application.js
+++ logic/application.js
@@ -32,8 +32,6 @@ let pullingImages = false; // Is the manager currently pulling images
 let systemStatus;
 resetSystemStatus();
 
-const logArchiveLocalPath = constants.WORKING_DIRECTORY + '/' + constants.NODE_LOG_ARCHIVE;
-const logArchiveLocalPathTemp = constants.WORKING_DIRECTORY + '/' + constants.NODE_LOG_ARCHIVE_TEMP;
 const MAX_RESYNC_ATTEMPTS = 5;
 
 function resetSystemStatus() {
@@ -655,43 +653,6 @@ async function wipeSettingsVolume() {
   await bashService.exec('rm', ['-f', 'settings.json'], options);
 }
 
-// Launch docker container which will tar logs.
-async function downloadLogs() {
-  const logArchiveBackupPath = '/backup/' + constants.NODE_LOG_ARCHIVE_TEMP;
-
-  const backUpCommandOptions = [
-    'run',
-    '--rm',
-    '-v', 'applications_logs:/logs',
-    '-v', constants.WORKING_DIRECTORY.concat(':/backup'),
-    'alpine',
-    'tar', '-cjf', logArchiveBackupPath, '-C', '/logs', './'
-  ];
-
-  await bashService.exec('docker', backUpCommandOptions, {});
-
-  const gpgCommandOptions = [
-    '--batch', '--yes', // allow overwriting.
-    '--output', logArchiveLocalPath,
-    '-r', constants.NODE_LOG_ARCHIVE_GPG_RECIPIENT,
-    '--trust-model', 'always', // TODO: Can we register our GPG public key with some CA?
-    '--encrypt', logArchiveLocalPathTemp
-  ];
-
-  await bashService.exec('gpg', gpgCommandOptions, {});
-
-  return logArchiveLocalPath;
-}
-
-// Remove log archive.
-function deleteLogArchives() {
-  const options = {
-    cwd: constants.WORKING_DIRECTORY
-  };
-
-  bashService.exec('rm', ['-f', logArchiveLocalPath, logArchiveLocalPathTemp], options);
-}
-
 // Compare known compose files, except manager.yml, with on-device YMLs.
 // The manager should have the latest YMLs.
 async function checkYMLs() {
@@ -882,8 +843,6 @@ async function login(user) {
 }
 
 module.exports = {
-  downloadLogs,
-  deleteLogArchives,
   getSerial,
   getSystemStatus,
   getFilteredVersions,
